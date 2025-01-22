57c842c549e57f19259300be39d5c7eaa90355b1
Professor-Embedded <61ashishparmar@gmail.com>
2025-01-22 13:27:57 +0530

    Created classes.py

---
+++ classes.py
@@ -0,0 +1,25 @@
+# lst = []
+# lst = [x for x in range(12 21):]
+# print(lst)
+'''
+class Product:
+    def __init__(self):
+        self.name = 'Iphone'
+        self.description = 'Its awesome'
+        self.price = 700
+p1 = Product()
+p1.price = 400
+print(p1.price)
+
+class Course:
+    def __init__(self, name, ratings):
+        self.name = name
+        self.ratings = ratings
+        
+        
+c1 = Course('C++', 4.8)
+print(c1.name)
+print(c1.ratings)
+
+'''
+
